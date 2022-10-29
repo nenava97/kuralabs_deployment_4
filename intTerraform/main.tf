75880e9593923334ef8c702f9a959fd374463703
nenava97 <108698688+nenava97@users.noreply.github.com>
2022-10-28 21:58:18 -0400

    Update main.tf

--- intTerraform/main.tf
+++ intTerraform/main.tf
@@ -19,7 +19,7 @@ resource "aws_instance" "web_server01" {
   instance_type = "t2.micro"
   key_name = "key3"
   subnet_id = "subnet-079f4b67d601bcb68"
-  vpc_security_group_ids = "sg-07068b33d71e121a6"
+ vpc_security_group_ids = [aws_security_group.web_ssh.id]
 
   user_data = "${file("deploy.sh")}"
 
