7c51e0daefac9a59e69867e276c775a415503d8f
nenava97 <108698688+nenava97@users.noreply.github.com>
2022-10-28 21:39:23 -0400

    Update main.tf

--- intTerraform/main.tf
+++ intTerraform/main.tf
@@ -8,6 +8,12 @@ provider "aws" {
   
 }
 
+variable "vpc_id" {}
+
+data "aws_vpc" "selected" {
+  id = var.vpc_id
+}
+
 resource "aws_instance" "web_server01" {
   ami = "ami-08c40ec9ead489470"
   instance_type = "t2.micro"
