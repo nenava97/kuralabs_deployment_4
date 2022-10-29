c06081acbb4ebf1649035436fa4e320092f49f42
nenava97 <108698688+nenava97@users.noreply.github.com>
2022-10-28 21:15:15 -0400

    Update main.tf

--- intTerraform/main.tf
+++ intTerraform/main.tf
@@ -12,7 +12,8 @@ resource "aws_instance" "web_server01" {
   ami = "ami-08c40ec9ead489470"
   instance_type = "t2.micro"
   key_name = "key3"
-  vpc_security_group_ids = [aws_security_group.web_ssh.id]
+  subnet_id = "subnet-079f4b67d601bcb68"
+  vpc_security_group_ids = [aws_security_group.var.vpc_id]
 
   user_data = "${file("deploy.sh")}"
 
@@ -27,9 +28,4 @@ output "instance_ip" {
   
 }
 
-variable "vpc_id" {}
-
-data "aws_vpc" "selected" {
-  id = var.vpc_id
-}
 
