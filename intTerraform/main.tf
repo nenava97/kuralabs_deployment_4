ad10a57565eaf1b40aaaa85fd5016bee53998801
nenava97 <108698688+nenava97@users.noreply.github.com>
2022-11-01 00:04:51 -0400

    Update main.tf

--- intTerraform/main.tf
+++ intTerraform/main.tf
@@ -8,18 +8,53 @@ provider "aws" {
   
 }
 
-variable "vpc_id" {}
-
-data "aws_vpc" "selected" {
-  id = var.vpc_id
+resource “aws_vpc” “prod-vpc” {
+    cidr_block = “10.0.0.0/16”
+    enable_dns_support = “true” #gives you an internal domain name
+    enable_dns_hostnames = “true” #gives you an internal host name
+    enable_classiclink = “false”
+    instance_tenancy = “default”    
+    
+    tags {
+        Name = “prod-vpc”
+    }
+}
+resource "aws_internet_gateway" "prod-igw" {
+    vpc_id = "${aws_vpc.prod-vpc.id}"
+    tags {
+        Name = "prod-igw"
+    }
+}
+resource “aws_subnet” “prod-subnet-public-1” {
+    vpc_id = “${aws_vpc.prod-vpc.id}”
+    cidr_block = “10.0.1.0/24”
+    map_public_ip_on_launch = “true” //it makes this a public subnet
+    availability_zone = “eu-east-1a”
+    tags {
+        Name = “prod-subnet-public-1”
+    }
+}
+resource "aws_route_table" "prod-public-crt" {
+    vpc_id = "${aws_vpc.main-vpc.id}"
+    
+    route {
+        //associated subnet can reach everywhere
+        cidr_block = "0.0.0.0/0" 
+        //CRT uses this IGW to reach internet
+        gateway_id = "${aws_internet_gateway.prod-igw.id}" 
+    }
+    
+    tags {
+        Name = "prod-public-crt"
+    }
 }
 
 resource "aws_instance" "web_server01" {
   ami = "ami-08c40ec9ead489470"
   instance_type = "t2.micro"
   key_name = "key3"
-  subnet_id = "subnet-079f4b67d601bcb68"
- vpc_security_group_ids = [aws_security_group.web_ssh.id]
+  subnet_id = "${aws_subnet.prod-subnet-public-1.id}"
+  vpc_security_group_ids = [aws_security_group.web_ssh.id]
 
   user_data = "${file("deploy.sh")}"
 
