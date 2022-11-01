5c0ce0ff711225448522cb161d6e3bf07ed3ad35
nenava97 <108698688+nenava97@users.noreply.github.com>
2022-11-01 16:35:52 -0400

    Update main.tf

--- intTerraform/main.tf
+++ intTerraform/main.tf
@@ -34,14 +34,17 @@ resource "aws_subnet" "prod-subnet-public-1" {
 }
 resource "aws_route_table" "prod-public-crt" {
     vpc_id = "${aws_vpc.prod-vpc.id}"
-    
     route {
         //associated subnet can reach everywhere
         cidr_block = "0.0.0.0/0" 
         //CRT uses this IGW to reach internet
         gateway_id = "${aws_internet_gateway.prod-igw.id}" 
     }
-    
+      # Route table Association with Public Subnet's
+ resource "aws_route_table_association" "PublicRTassociation" {
+    subnet_id = aws_subnet.prod-subnet-public-1.id
+    route_table_id = aws_route_table.prod-public-crt.id
+ }
     tags = {
         "Name" : "prod-public-crt"
     }
