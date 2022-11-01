8331109de59c0452a0bb6972b0a7a3b1d1d7608d
nenava97 <108698688+nenava97@users.noreply.github.com>
2022-11-01 01:45:59 -0400

    Update SG.tf

--- intTerraform/SG.tf
+++ intTerraform/SG.tf
@@ -12,13 +12,14 @@ resource "aws_security_group" "web_ssh" {
   }
 
   ingress {
-    from_port = 8000
+    from_port = 5000
     to_port = 8000
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
 
   }
 
+  }
   egress {
     from_port = 0
     to_port = 0
